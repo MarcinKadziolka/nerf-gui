48ef4c73bc81e1a3364417d0f6275efda54be5bf
Marcin Kadziolka <kadziolka.marcin@outlook.com>
2024-05-10 16:50:29 +0200

    Add displaying test image and buttons for arrows

--- nerf_gui.py
+++ nerf_gui.py
@@ -1,8 +1,10 @@
 import pygame
 import settings
 from classes import (
+    ButtonLayout,
     CheckBoxLayout,
     Orientation,
+    Image,
 )
 import os
 
@@ -16,16 +18,16 @@ def lego():
     run = True
     checkboxes_x = settings.SCREEN_SIZE.right_third
     checkboxes_y = 600
+    small_button_width = 60
     n_samples = CheckBoxLayout(
         ["8", "16", "32", "64", "128"],
         active={0},
-        width=60,
+        width=small_button_width,
         distance=settings.DISTANCE,
         x=checkboxes_x,
         y=checkboxes_y,
         orientation=Orientation.HORIZONTAL,
     )
-
     ablation = CheckBoxLayout(
         ["Pos encoding", "View direction"],
         active={0, 1},
@@ -36,17 +38,41 @@ def lego():
         orientation=Orientation.VERTICAL,
         multiple_choice=True,
     )
-
+    image = Image(
+        image_path="000.png",
+        scale_factor=1.5,
+        x=int(settings.SCREEN_SIZE.x * 2 / 6),
+        y=settings.SCREEN_SIZE.mid_y - 50,
+    )
+    arrows = ButtonLayout(
+        ["<", ">"],
+        active={0, 1},
+        width=small_button_width,
+        distance=settings.DISTANCE,
+        orientation=Orientation.HORIZONTAL,
+        x=image.x,
+        y=int(image.y + image.height / 2 + settings.DISTANCE),
+    )
     while run:
         screen.fill(settings.Color.BACKGROUND.value)
         for event in pygame.event.get():
             n_samples.update(event)
             ablation.update(event)
+
+            if (activated_arrow := arrows.update(event)) is not None:
+                if activated_arrow.text == "<":
+                    pass
+                elif activated_arrow.text == ">":
+                    pass
+
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     run = False
             if event.type == pygame.QUIT:
                 run = False
+
+        image.draw(screen)
+        arrows.display(screen)
         n_samples.display(screen)
         ablation.display(screen)
         pygame.display.update()
